bc91b136e79bbb465f7fb082173f2f7360861f90
HaoNYC <haoxuanz6@nycstudents.net>
2026-04-27 03:14:36 +0000

    up

---
+++ Lesson-51-Classwork/Lesson51Classwork/script.js
@@ -0,0 +1,47 @@
+
+let employees;
+function init(){
+  $.ajaxSetup({async: false});
+  
+  let link = "https://Lesson48ClassworkDBServer.ernestoporchett.repl.co";
+  let route= "/employees"
+  employees = $.getJSON(link+route).responseJSON;
+
+  generateCards(employees);
+
+  
+}
+
+function generateCards(employees){
+
+  let output = document.getElementById("output");
+  let build ="";
+
+   
+  for(let i=0; i<employees.length; i++){
+    let employee = employees[i]
+    build += `<div class="card" >`
+    build += `<h3> Employee ID : ${employee.EmployeeId}</h3>`;
+    build += `<div> First Name : ${employee.FirstName}</div>`;
+    build += `<div> Last Name : ${employee.LastName}</div>`;
+    build += `<p> City : ${employee.City}</p>`;
+    build += `<hr>`;
+    build += `</div>`;
+  }
+
+  output.innerHTML = build;
+  
+}
+
+function filter() {
+  let citySearch = document.getElementById("City").value;
+  let newEmployees = [];
+  for (let i = 0; i < employees.length; i++) {
+    let employee = employees[i];
+    if (employee.City.includes(citySearch)) {
+      newEmployees.push(employee);
+    }
+  }
+
+  generateCards(newEmployees);
+}
