bacf38ed6063d850c651f415e41e7253b0408cf7
HaoNYC <haoxuanz6@nycstudents.net>
2026-05-01 01:53:59 +0000

    up

--- Lesson56Classwork/employees.js
+++ Lesson56Classwork/employees.js
@@ -3,7 +3,7 @@ let employees;
 function init(){
   $.ajaxSetup({async: false});
   
-  let link = "https://Lesson48ClassworkDBServer.ernestoporchett.repl.co";
+  let link = "https://raw.githubusercontent.com/PorchettaEP/JSONFILES/refs/heads/main";
   let route= "/employees"
   employees = $.getJSON(link+route).responseJSON;
 
@@ -16,6 +16,26 @@ function generateCards(employees){
 
   let mainpanel = document.getElementById("centerpanel");
   
+  // Clear previous cards
+  mainpanel.innerHTML = "";
+  
+  for(let i = 0; i < employees.length; i++){
+    let employee = employees[i];
+    
+    // Create text for the button
+    let text = `${employee.EmployeeID} - ${employee.FirstName} ${employee.LastName}`;
+    
+    // Create content with city and flag
+    let content = `<h3>${employee.City}</h3>`;
+    content += `<img src='../cities/${employee.City}.PNG' style='width:100px; height:auto;'>`;
+    
+    // Create and render modal
+    let modal = new Modal(text, content);
+    modal.render("centerpanel");
+  }
+  
+  // Update count
+  document.getElementById("count").innerHTML = `Total: ${employees.length} employees`;
 }
 
 function filter(){
--- Lesson56Classwork/script.js
+++ Lesson56Classwork/script.js
@@ -2,7 +2,7 @@ let data,customers;
 function init(){
   $.ajaxSetup({async: false});
   
-  let link = "https://Lesson48DemoDBServer.ernestoporchett.repl.co";
+  let link = "https://raw.githubusercontent.com/PorchettaEP/JSONFILES/refs/heads/main";
   let route= "/customers"
   customers = $.getJSON(link+route).responseJSON;
 
@@ -13,10 +13,29 @@ function init(){
 
 function generateCards(customers){
   let centerpanel = document.getElementById("centerpanel");
-
-
+  
+  // Clear previous cards
+  centerpanel.innerHTML = "";
+  
+  for(let i = 0; i < customers.length; i++){
+    let customer = customers[i];
     
-
+    // Create front of card with customer info
+    let front = `<h3>Customer ID: ${customer.CustomerID}</h3>`;
+    front += `<p><strong>${customer.FirstName} ${customer.LastName}</strong></p>`;
+    front += `<p>Email: ${customer.Email}</p>`;
+    
+    // Create back of card with country and flag
+    let back = `<h3>${customer.Country}</h3>`;
+    back += `<img src='countries/${customer.Country}.PNG' style='width:100px; height:auto;'>`;
+    
+    // Create and render flip card
+    let card = new FlipCard(front, back);
+    card.render("centerpanel");
+  }
+  
+  // Update count
+  document.getElementById("count").innerHTML = `Total: ${customers.length} customers`;
 }
 
 function filter(){
